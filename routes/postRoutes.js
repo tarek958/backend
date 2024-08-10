5e80f249c1af1e032e49dfcd7a3cd0c1cab188f7
tarek958 <tarekdhokkar@gmail.com>
2024-08-10 02:25:16 +0100

    update

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 const authenticateToken = require('../middleware/authenticateToken');
-router.post('/add',authenticateToken, postController.createPost);
+router.post('/add/', authenticateToken, postController.createPost);
 router.get('/all',authenticateToken, postController.getAllPosts);
 router.get('/:id',authenticateToken, postController.getPostById);
 router.put('/:id', authenticateToken,postController.updatePostById);
